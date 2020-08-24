2cbbaf3aa657470b51e30164de5f57deb8c9e80f
DawnLuna <xjy.pub@gmail.com>
2020-08-23 23:22:47 -0400

    fix err for addSection

--- src/controllers/forumController.js
+++ src/controllers/forumController.js
@@ -4,7 +4,7 @@ export const addSection = (req, res) => {
     let newSection = new Section(req.body);
     newSection.save((err, section) => {
         if (err) {
-            res.send(err.massage);
+            res.send({ succes: false, massage: err.massages });
         } else {
             section.__v = undefined;
             section.hidden = undefined;
