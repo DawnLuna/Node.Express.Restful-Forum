c49a117d4f00a3750e0abbd0a44ad70973bfbb78
DawnLuna <xjy.pub@gmail.com>
2020-09-02 23:34:41 -0400

    +signature/threadCount/replyCount to UserSchema

--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -5,6 +5,9 @@ const UserSchema = new Schema({
     username: { type: String, required: true, unique: true, index: true },
     email: { type: String, required: true, unique: true, index: true },
     password: { type: String, required: true },
+    signature: { type: String, default: '' },
+    threadCount: { type: Number, default: 0 },
+    replyCount: { type: Number, default: 0 },
     banned: { type: Boolean, default: false }
 },
     { timestamps: true }
